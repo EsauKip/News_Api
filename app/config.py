e59e8ac92c203a727f656d996372036e4ff6562a
EsauKip <esau.kiprono@student.moringaschool.com>
2022-02-27 01:28:16 +0300

    add nav bar,add,links ,import bootstrap,send requests

--- app/config.py
+++ app/config.py
@@ -1,9 +1,14 @@
+
 class Config:
     '''
     General configuration parent class
     '''
     NEWS_BASE_URL = 'https://newsapi.org/v2/everything?q=bitcoin&apiKey={}'
 
+    NEWS_SOURCE_URL = 'https://newsapi.org/v2/sources?language=en&category={}&apiKey={}'
+    # NEWS_TOP_HEADLINE_URL = 'https://newsapi.org/v2/top-headlines?country=us&apiKey={}'
+    CAT_API_URL='https://newsapi.org/v2/sources?language=en&category={}&apiKey={}'
+    # NEWS_API_KEY=os.environ.get('NEWS_API_KEY')
 
 
 class ProdConfig(Config):
--- app/views.py
+++ app/views.py
@@ -1,11 +1,34 @@
-from flask import render_template
-from app import app
 
-# Views
+from . import app
+from flask import render_template
+from .request import get_source,article_source,get_category,get_headlines
+
+#our views
 @app.route('/')
 def index():
+    '''
+    Root function returning index/home page with data
+    '''
+    source= get_source('business')
+    headlines = get_headlines()
+    return render_template('index.html',sources=source,headlines=headlines)
+@app.route('/article/<id>')
+def article(id):
 
     '''
-    View root page function that returns the index page and its data
+    View article page function that returns the various article details page and its data
     '''
-    return render_template('index.html')
+    # title= 'Articles'
+    articles = article_source(id)
+    return render_template('article.html',articles= articles,id=id )
+
+@app.route('/categories/<cat_name>')
+def category(cat_name):
+    '''
+    function to return the categories.html page and its content
+    '''
+    category = get_category(cat_name)
+    title = f'{cat_name}'
+    cat = cat_name
+
+    return render_template('categories.html',title = title,category = category, cat= cat_name)
