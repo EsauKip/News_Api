e181e93e67e8884ef7b16d6064eb960d0b908caa
EsauKip <esau.kiprono@student.moringaschool.com>
2022-02-26 10:51:12 +0300

    update classes ,add imports to the init .py file ,add function returning homepage with data

--- app/main/views.py
+++ app/main/views.py
@@ -2,3 +2,11 @@ from flask import render_template,request,redirect,url_for
 from . import main
 from flask import render_template,request,redirect,url_for
 from ..request import get_source,article_source,get_category,get_headlines
+#our view
+@main.route('/')
+def index():
+    """return home page with data
+    """
+    sources = get_source
+    headlines=get_headlines
+    return render_template('index.html', sources=sources, headlines=headlines)
--- app/models.py
+++ app/models.py
@@ -7,4 +7,32 @@ def __init__(self,author,description,time,url,image,title):
     self.time = time
     self.url = url
     self.image = image
+    self.title = title 
+class source:
+    """class that instanciates the source object
+    """
+def __init__(self,name,description,id,url):   
+    self.name = name
+    self.description = description
+    self.id = id
+    self.url = url
+class Headlines:
+    """class that instanciates the headlines of the categories and sources object
+    """   
+def __init__(self,author,description,time,url,image,title):
+    self.author = author
+    self.description = description
+    self.time = time
+    self.url = url
+    self.image = image
     self.title = title    
+class Category:
+    """class that instanciates object news  categories object from the sources class
+    """
+def __init__(self,author,description,time,url,image,title):
+    self.author = author
+    self.description = description
+    self.time = time
+    self.url = url
+    self.image = image
+    self.title = title
