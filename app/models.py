2a102f6364a9963ba86fac4cfdbfd837a722d36a
EsauKip <esau.kiprono@student.moringaschool.com>
2022-02-26 09:32:53 +0300

    add class atrticles to the models.py file

---
+++ app/models.py
@@ -0,0 +1,10 @@
+class Article:
+    """class that instanciates news articles from the news api
+    """
+def __init__(self,author,description,time,url,image,title):
+    self.author = author
+    self.description = description
+    self.time = time
+    self.url = url
+    self.image = image
+    self.title = title    
